a102ec23d123496be634f2cb44a3f9adf0fb74e0
Ines Montani <ines@ines.io>
2019-09-30 22:00:32 +0200

    Update tests

---
+++ spacy_lookups_data/tests/test_en.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from spacy.lang.en import English
+import pytest
+
+
+@pytest.fixture(scope="session")
+def en_nlp():
+    return English()
+
+
+@pytest.mark.parametrize("text", ["faster", "fastest", "better", "best"])
+def test_en_lemmatizer_handles_irreg_adverbs(en_nlp, text):
+    tokens = en_nlp(text)
+    assert tokens[0].lemma_ in ["fast", "well"]
+
+
+def test_issue4104(en_nlp):
+    """Test that English lookup lemmatization of spun & dry are correct
+    expected mapping = {'dry': 'dry', 'spun': 'spin', 'spun-dry': 'spin-dry'}
+    """
+    doc = en_nlp("dry spun spun-dry")
+    assert [token.lemma_ for token in doc] == ["dry", "spin", "spin-dry"]
