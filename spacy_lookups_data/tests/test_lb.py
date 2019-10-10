bf309539f8015c300b68aed9710fc4449c67ebc9
Peter Gilles <PeterGilles@users.noreply.github.com>
2019-10-10 17:28:24 +0200

    lemma lookup for Luxembourgish

---
+++ spacy_lookups_data/tests/test_lb.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from spacy.lang.lb import Luxembourgish
+import pytest
+
+
+@pytest.fixture(scope="session")
+def lb_nlp():
+    return Luxembourgish()
+
+
+@pytest.mark.parametrize(
+    "string,lemma",
+    [
+        ("Dëscher", "Dësch"),
+        ("engagéiers", "engagéieren"),
+        ("goung", "goen"),
+        ("neit", "nei"),
+        ("déi", "déi"),
+        ("Männer", "Mann"),
+        ("Mënner", "Mond"),
+        ("kritt", "kréien")
+    ],
+)
+def test_lb_lemmatizer_lookup_assigns(lb_tokenizer, string, lemma):
+    tokens = lb_tokenizer(string)
+    assert tokens[0].lemma_ == lemma
