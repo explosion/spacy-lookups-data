191d9308ab34e9c082de3bda6da30866f8e0e137
Adriane Boyd <adrianeboyd@gmail.com>
2020-04-22 21:47:56 +0200

    Move norm exceptions tests from spacy

--- spacy_lookups_data/tests/test_da.py
+++ spacy_lookups_data/tests/test_da.py
@@ -22,3 +22,11 @@ def da_nlp():
 def test_da_lemmatizer_lookup_assigns(da_nlp, string, lemma):
     tokens = da_nlp(string)
     assert tokens[0].lemma_ == lemma
+
+
+@pytest.mark.parametrize(
+    "text,norm", [("akvarium", "akvarie"), ("bedstemoder", "bedstemor")]
+)
+def test_da_nlp_norm_exceptions(da_nlp, text, norm):
+    tokens = da_nlp(text)
+    assert tokens[0].norm_ == norm
--- spacy_lookups_data/tests/test_de.py
+++ spacy_lookups_data/tests/test_de.py
@@ -24,3 +24,16 @@ def de_nlp():
 def test_de_lemmatizer_lookup_assigns(de_nlp, string, lemma):
     tokens = de_nlp(string)
     assert tokens[0].lemma_ == lemma
+
+@pytest.mark.parametrize(
+    "text,norms", [("vor'm", ["vor", "dem"]), ("du's", ["du", "es"])]
+)
+def test_de_nlp_norm_exceptions(de_nlp, text, norms):
+    tokens = de_nlp(text)
+    assert [token.norm_ for token in tokens] == norms
+
+
+@pytest.mark.parametrize("text,norm", [("daß", "dass")])
+def test_de_lex_attrs_norm_exceptions(de_nlp, text, norm):
+    tokens = de_nlp(text)
+    assert tokens[0].norm_ == norm
