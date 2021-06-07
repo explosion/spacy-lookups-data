5e9b52dbf00b2b74e14603b7b49a2a31a9647b16
Adriane Boyd <adrianeboyd@gmail.com>
2021-06-07 14:25:39 +0200

    Clean up

--- spacy_lookups_data/tests/test_ru.py
+++ spacy_lookups_data/tests/test_ru.py
@@ -9,7 +9,5 @@ import pytest
     ],
 )
 def test_lemmatizer_lookup_assigns(ru_lookup_nlp, string, lemma):
-    print(string, lemma)
     tokens = ru_lookup_nlp(string)
-    print([t.text for t in tokens], [t.lemma_ for t in tokens])
     assert tokens[0].lemma_ == lemma
