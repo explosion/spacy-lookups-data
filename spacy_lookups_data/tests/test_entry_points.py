f54c5896a06f27915382ea958f2d4239d167ab39
Adriane Boyd <adrianeboyd@gmail.com>
2021-06-11 11:56:38 +0200

    Fix file check in test

--- spacy_lookups_data/tests/test_entry_points.py
+++ spacy_lookups_data/tests/test_entry_points.py
@@ -6,4 +6,4 @@ def test_all_entry_points():
     entry_points = spacy.registry.lookups.get_all()
     for lang, tables in spacy.registry.lookups.get_all().items():
         for name, filename in tables.items():
-            assert os.path.exists(filename)
+            spacy.util.load_language_data(filename)
