f3cea5ebcca97c4fd7be107930dbf7c2270eed31
Jacobo <jcbmyrstn@gmail.com>
2021-07-15 21:16:18 -0700

    add lookup tables for ancient Greek and entry points

---
+++ spacy_lookups_data/tests/test_grc.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    "string,lemma",
+    [
+        ("ἄνδρα", "ἀνήρ"),
+        ("μοι", "ἐγώ"),
+        ("πολύτροπον", "πολύτροπος"),
+        ("ὃς", "ὅς"),
+        ("πολλὰ", "πολύς"),
+        ("καρδίαν", "καρδία"),
+    ],
+)
+def test_grc_lemmatizer_lookup_assigns(grc_nlp, string, lemma):
+    tokens = grc_nlp(string)
+    assert tokens[0].lemma_ == lemma
