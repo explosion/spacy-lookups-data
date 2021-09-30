f50a4ffd2bd2a0058610cfa95766a93d09caaaff
Adriane Boyd <adrianeboyd@gmail.com>
2021-09-30 14:25:28 +0200

    Fix type of ga lemmatizer in tests

--- spacy_lookups_data/tests/test_ga.py
+++ spacy_lookups_data/tests/test_ga.py
@@ -2,9 +2,9 @@ import pytest
 
 
 @pytest.mark.skip(reason="Enable after spaCy v3.2.0 release")
-def test_ga_lemmatizer(ga_lookup_nlp):
-    doc = Doc(ga_lookup_nlp.vocab, words=["chonaic", "abacais", "abaraigh"], pos=["VERB", "NOUN", "ADJ"])
-    doc = ga_lookup_nlp.get_pipe("lemmatizer")(doc)
+def test_ga_lemmatizer(ga_pos_lookup_nlp):
+    doc = Doc(ga_pos_lookup_nlp.vocab, words=["chonaic", "abacais", "abaraigh"], pos=["VERB", "NOUN", "ADJ"])
+    doc = ga_pos_lookup_nlp.get_pipe("lemmatizer")(doc)
     assert doc[0].lemma_ == "feic"
     assert doc[1].lemma_ == "abacas"
     assert doc[2].lemma_ == "abarach"
